39dce67778c8d10aa6f59f23771ae22d22fec279
Tungpham201201 <botmei312@gmail.com>
2024-08-09 13:31:38 +0700

    Update h.py

--- h.py
+++ h.py
@@ -27,7 +27,12 @@ class Zefoy:
         self.services_status = {}
         self.url = 'None'
         self.text = 'Tool Zefoy'
-	url1=input("[bold green]♕ TIKTOK:[/bold green][bold purple] 🔱ROASTED_KING GG 🔱.[/bold purple]\n[bold green]♕ Telegram[/bold green]: [bold blue]@Roasted2001[/bold blue]\n[bold green]♕ Zalo[/bold green]: [bold blue]0983544223[/bold blue]\n[bold green]♕ Youtube[/bold green]: [bold blue]@roasted_2001[/bold blue]\n[bold red]==================================================[/bold red]\n\033[1;33m-> link video:  ")
+	url1=input("[bold green]♕ TIKTOK:[/bold green][bold purple] 🔱ROASTED_KING GG 🔱.[/bold purple]",
+		   "[bold green]♕ Telegram[/bold green]: [bold blue]@Roasted2001[/bold blue]",
+		   "[bold green]♕ Zalo[/bold green]: [bold blue]0983544223[/bold blue]",
+		   "[bold green]♕ Youtube[/bold green]: [bold blue]@roasted_2001[/bold blue]",
+		   "[bold red]==================================================[/bold red]",
+		   "\033[1;33m-> link video:  ")
       
         self.url=url1
 
